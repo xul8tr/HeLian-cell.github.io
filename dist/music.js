abcac8c40c3f511d774eecc8da92b5ce13d66b80
helian-cell <2686541713@qq.com>
2020-02-15 15:48:36 +0800

    Site updated: 2020-02-15 15:48:33

--- dist/music.js
+++ dist/music.js
@@ -4,13 +4,13 @@ const ap = new APlayer({
     autoplay: true, //自动播放
     theme: '#FADFA3', //主题色
     loop: 'all', //音频循环播放, 可选值: 'all'全部循环, 'one'单曲循环, 'none'不循环
-    order: 'random', //音频循环顺序, 可选值: 'list'列表循环, 'random'随机循环
+    order: 'list', //音频循环顺序, 可选值: 'list'列表循环, 'random'随机循环
     preload: 'auto', //预加载，可选值: 'none', 'metadata', 'auto'
     volume: 0.7, //默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效
     mutex: true, //互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器
     listFolded: false, //列表默认折叠
     listMaxHeight: 90, //列表最大高度
-    lrcType: 0, //歌词传递方式
+    lrcType: 3, //歌词传递方式
     audio: [
 	{
         name: '前前世世',
